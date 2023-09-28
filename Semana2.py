a689cf8518d3906f00386423e82ff91a023a0403
LucasMDoval <martindovallucas@gmail.com>
2023-09-28 12:35:34 +0200

    Create Semana2.py

---
+++ Semana2.py
@@ -0,0 +1,13 @@
+temperature_celsius = 30 
+texto1 = "La temperature en celsius es:"
+
+print(texto1, temperature_celsius)
+
+temperature_fahrenheit = ((temperature_celsius*9/5)+32)
+
+texto2 = "La temperatura en Fahrenheit es :"
+
+print (texto2, temperature_fahrenheit)
+
+
+
