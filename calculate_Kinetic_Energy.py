ee9a768d35e6fdc932b4b8ed58c9035fe2bb4509
LucasMDoval <martindovallucas@gmail.com>
2023-09-28 13:09:46 +0200

    Create calculate_Kinetic_Energy.py

---
+++ calculate_Kinetic_Energy.py
@@ -0,0 +1,7 @@
+
+mass = float(input("Enter the mass (in kilograms) :"))
+velocity = float(input("Enter the velocity (in m/s) :"))
+
+kinetic_energy = (0.5*mass*(velocity**2))
+
+print ("The kinetic energy is : ",kinetic_energy)
